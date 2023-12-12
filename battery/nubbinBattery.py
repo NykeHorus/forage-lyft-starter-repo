445a090cbdc835493d561c5c1d3751d38ab976d2
Hariss <harissbilal@hotmail.com>
2023-12-12 18:46:11 +0500

    refactored

--- battery/nubbinBattery.py
+++ battery/nubbinBattery.py
@@ -1,13 +1,14 @@
-from datetime import datetime
+from utili import add_years_to_date
 from battery.battery import Battery
 
 class NubbenBattery(Battery):
     def __init__(self, last_service_date, current_date):
-        self.last_service_date= last_service_date
+        self.last_service_date = last_service_date
         self.current_date = current_date
         
     def needs_service(self):
-        if self.last_service_date - self.current_date > 1460:
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 4)
+        if date_which_battery_should_be_serviced_by < self.current_date:
             return True
         else: 
             return False
